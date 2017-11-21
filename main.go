ace02fa4079ccef941bdaf925335506ee1bec5b2
wingillis <win.gillis@gmail.com>
2017-11-21 10:29:28 -0500

    Can read file paths from config file
    also added comments for readability

--- main.go
+++ main.go
@@ -3,34 +3,51 @@ package main
 import (
 	"log"
 	"path/filepath"
+	"encoding/json"
 	"os"
 	"bytes"
+	"io/ioutil"
 	"crypto/sha1"
 	"github.com/fsnotify/fsnotify"
 )
 
-const comparePath = "/Users/wgillis/Dropbox (HMS)/lab-datta/papers to read"
-const watchDir = "/Users/wgillis/Dropbox (HMS)/mendeley"
+type WatcherStruct struct {
+	To string
+	From string
+	Watch string
+}
 
 func main() {
+	var paths WatcherStruct
+	f, err := ioutil.ReadFile("watcherConfig.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(f, &paths)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// create object to watch files from specific directory
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
-	// archive, err := filepath.Abs(filepath.Join(comparePath, ".."))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer watcher.Close() // this runs after main() exits
 
+	// run function in another channel
 	done := make(chan bool)
 	go func() {
 		for { // runs like a while loop in other langs
 			select {
 			case event := <-watcher.Events:
+				// run this stuff when a new file is created
 				if event.Op&fsnotify.Create == fsnotify.Create {
-					dirs, erro := filepath.Glob(filepath.Join(comparePath, "*.pdf"))
+					// list all files in the input file path
+					dirs, erro := filepath.Glob(filepath.Join(paths.From, "*.pdf"))
 					if erro != nil {
 						log.Fatal(erro)
 					}
@@ -38,15 +55,20 @@ func main() {
 					if err != nil {
 						log.Fatal(err)
 					}
+					// read in 4kb of data to compute a checksum
 					filebytes := make([]byte, 4096)
 					_, err = file.Read(filebytes)
 					if err != nil {
 						log.Fatal(err)
 					}
-					chksum := sha1.Sum(filebytes)
 					file.Close()
+					// compute the checksum
+					chksum := sha1.Sum(filebytes)
 					notequal := true
+					// loop thru each file in input path until a file matches the checksum of the
+					// created file in the mendeley directory
 					for i := 0; i < len(dirs) && notequal; i++ {
+						// open file and read 4kb
 						f2, err := os.Open(dirs[i])
 						if err != nil {
 							log.Fatal(err)
@@ -57,11 +79,13 @@ func main() {
 							log.Fatal(err)
 						}
 						f2.Close()
+						// compute checksum
 						chksum2 := sha1.Sum(fb2)
+						// compare the bytes themselves
 						if bytes.Equal(chksum2[:], chksum[:]) {
 							log.Println("These two files are equal:", dirs[i], event.Name)
 							notequal = true
-							newPath := filepath.Join(comparePath, "..", "paper-archive", filepath.Base(dirs[i]))
+							newPath := filepath.Join(paths.To, filepath.Base(dirs[i]))
 							log.Println("Moving", dirs[i], "to", newPath)
 							os.Rename(dirs[i], newPath)
 						}
@@ -74,7 +98,8 @@ func main() {
 		}
 	}()
 
-	err = watcher.Add(watchDir)
+	// add the mendeley dir to watch for created files
+	err = watcher.Add(paths.Watch)
 	if err != nil {
 		log.Fatal(err)
 	}
